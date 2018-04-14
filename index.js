d67af1e5865b6619a0e07fde8a430cce4df290f2
alikoerniawan <alikoernaiwan02@gmail.com>
2018-04-15 02:32:13 +0700

    Update

--- index.js
+++ index.js
@@ -17,11 +17,34 @@ let inputListed = [];
 
 let inner1 = "";
 let viewsX = inner1.innerHTML = `
-<div>
-  <h1>Server is running Now</h1>
-  <h2>to Access API please use :</h2><a href=""><button type="submit">Client Area</button></a>
-  <img src="server2.png">
+<link rel="stylesheet" href="./bootstrap.min.css">
+<link rel="stylesheet" href="./index.css">
+
+<div class="jumbotron">
+  <h1 class="display-4">Server is Runnning Now !!</h1>
+  <p class="lead">You can access the Ali Kurniawan API.</p>
+  <hr class="my-4">
+
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Client API</a>
+  </p>
+
+  <div class="container">
+  <div class="row">
+    <div class="col ">
+      <img class="sizex" src="server2.png">
+    </div>
+
+  </div>
+  </div>
+
 </div>
+
+
+
+<script src="./bootstrap/jquery-slim.min.js"></script>
+<script src="./bootstrap/popper.min.js"></script>
+<script src="./bootstrap/bootstrap.min.js"></script>
 `;
 
 app.get('/', (req, res) => {
